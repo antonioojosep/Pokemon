1bba3cb8f44883489f2419f3ab91b4cb754d277f
Ambrosio03 <ambrosio0304@gmail.com>
2025-02-05 13:58:22 +0100

    Cambio en mainController para redirigir a /init

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/')]
 final class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route( name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[ROUTE('/init', name: 'app_init')]
+    public function init(): Response
+    {
+        return $this->render('mainChoose/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
